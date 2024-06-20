48aa7167480d58dc8edb91989ec09250df6eb661
Aseep10 <hasyimalhabsyi09@gmail.com>
2024-06-20 23:28:03 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Informatika sports <?php echo date('Y');?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
